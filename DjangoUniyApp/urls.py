138904ce5dcc766c8999522c7fa706860d506a26
unknown <SENA@SDQAPRSSQFSD009.FormacionSena.RED>
2021-08-31 17:39:13 -0500

    clase de html bootstrap

--- DjangoUniyApp/urls.py
+++ DjangoUniyApp/urls.py
@@ -4,9 +4,13 @@ from . import views
 
 urlpatterns = [
  path('', views.index, name='index'),
- path('estudiante/<str:pk>/',views.estudiante, name='estudiante'),
+ path('estudiante/<str:pk>/',views.estudiante_views, name='estudiante'),
 
 
  path('ejemplo/',views.ejemplo_views, name='ejemplo'),
+
+ path('Productos/',views.productos_views, name='productos'),
+
+ path('logo/',views.logo_views, name='logo'),
  
 ]
--- DjangoUniyApp/views.py
+++ DjangoUniyApp/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def index(request):
- context = {'clase': 'Aprendiendo Django'}
- return render(request, 'listado_estudiantes.html', context)
+    context = {'clase': 'Aprendiendo Django'}
+    return render(request, 'listado_estudiantes.html', context)
 
-def estudiante(request, pk):
- return HttpResponse("Soy el estudiante numero: " + str(pk))
+def estudiante_views(request, pk):
+    return HttpResponse("Soy el estudiante numero: " + str(pk))
 
 
 def ejemplo_views(request):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
- return render(request,'ejemplo.html',locals())
+    return render(request,'ejemplo.html',locals())
+
+def productos_views(request): 
+    lista = estudiante.objects.all()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+    return render(request,'productos.html',locals())
+
+def logo_views(request):
+    return render(request,'logo.html',locals())
+
+
